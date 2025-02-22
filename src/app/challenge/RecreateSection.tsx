ca081da90eb8a8b39d2f15b1017b9cbb02b2e632
Owen Prendergast <owenprendergast8@gmail.com>
2025-02-22 14:11:40 -0500

    complete left column (no functionality)

--- src/app/challenge/RecreateSection.tsx
+++ src/app/challenge/RecreateSection.tsx
@@ -12,7 +12,7 @@ interface RecreateSectionProps {
 const RecreateSection = (props: RecreateSectionProps) => {
   const { paintingWidth, paintingHeight, title, artist, painting } = props;
   return (
-    <div className="flex flex-col gap-20">
+    <div className="flex flex-col justify-between items-center my-2">
       {/* Header Bar */}
       <div className="w-full flex flex-row justify-between items-center">
         <div className="flex flex-row gap-3 items-center">
@@ -36,6 +36,7 @@ const RecreateSection = (props: RecreateSectionProps) => {
         />
       </div>
 
+      {/* Rest of the section */}
       <div className="flex flex-col gap-4 items-center">
         <p className="text-white font-rb font-bold text-2xl opacity-75">
           RECREATE THIS PIECE
@@ -47,20 +48,39 @@ const RecreateSection = (props: RecreateSectionProps) => {
               paintingWidth +
               "px] h-[" +
               paintingHeight +
-              "px] bg-black m-4"
+              "px] bg-black m-2"
             }
           >
             {painting}
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <p className="text-white font-rb font-bold text-lg opacity-75 text-center max-w-[300px]">
+          <p className="text-white font-rb font-bold text-lg opacity-75 text-center max-w-[320px]">
             {'"' + title + '"'}
           </p>
           <p className="text-white font-rb font-bold text-md opacity-75 text-center">
             {artist}
           </p>
         </div>
+
+        {/* Hex code section */}
+        <div className="grid grid-cols-3 w-[320px] justify-items-center">
+          <div className="w-12 h-6 bg-white" />
+          <div className="w-12 h-6 bg-white" />
+          <div className="w-12 h-6 bg-white" />
+        </div>
+
+        <p className="text-white font-rb font-bold text-sm opacity-75 text-left max-w-[320px]">
+          This is a great hint that will give you some guidance on how to solve
+          this problem using cool tailwind methods yippeeeeeeeeeee
+        </p>
+      </div>
+
+      {/* Bottom Buttoms */}
+      <div className="flex flex-row gap-5">
+        <Image src="map.svg" alt="fun fact" width={38} height={38} />
+        <Image src="questionbox.svg" alt="hint" width={38} height={38} />
+        <Image src="painticon.svg" alt="color swap" width={38} height={38} />
       </div>
     </div>
   );
